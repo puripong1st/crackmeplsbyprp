29d83fe983370ffe405a8b7a0e93c5e6750ebe81
prp <44913650+puripong1st@users.noreply.github.com>
2023-02-15 01:18:57 -0800

    Update pyiboot05_bootstrap.py

--- pyiboot05_bootstrap.py
+++ pyiboot05_bootstrap.py
@@ -1,6 +1,6 @@
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"img\\")
+ASSETS_PATH = OUTPUT_PATH / Path(r".img\\")
 
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
